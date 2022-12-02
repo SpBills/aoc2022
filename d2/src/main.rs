d205be93353a3bb19ab5c00d6621a2ddc3b8801d
SpBills <spencebills0@gmail.com>
2022-12-02 08:35:02 -0500

    Scuffed pt2 entry

--- d2/src/main.rs
+++ d2/src/main.rs
@@ -37,22 +37,53 @@ impl Decision {
             _ => 0,
         }
     }
+
+    fn decision_lose(&self) -> (u32, u32) {
+        match (self.0, self.1) {
+            (RPC::Rock, RPC::Rock) => (0, RPC::Scissors as u32),
+            (RPC::Rock, RPC::Paper) => (3, RPC::Rock as u32),
+            (RPC::Rock, RPC::Scissors) => (6, RPC::Paper as u32),
+            (RPC::Paper, RPC::Rock) => (0, RPC::Rock as u32),
+            (RPC::Paper, RPC::Paper) => (3, RPC::Paper as u32),
+            (RPC::Paper, RPC::Scissors) => (6, RPC::Scissors as u32),
+            (RPC::Scissors, RPC::Rock) => (0, RPC::Paper as u32),
+            (RPC::Scissors, RPC::Paper) => (3, RPC::Scissors as u32),
+            (RPC::Scissors, RPC::Scissors) => (6, RPC::Rock as u32),
+            _ => (0, 0),
+        }
+    }
+}
+
+fn part_1(decisions: Vec<Decision>) -> u32 {
+    let mut score: u32 = 0;
+
+    for decision in decisions {
+        score += decision.decision();
+    }
+
+    return score;
+}
+
+fn part_2(decisions: Vec<Decision>) -> u32 {
+    let mut score: u32 = 0;
+
+    for decision in decisions {
+        let d = decision.decision_lose();
+        score += d.0 + d.1;
+    }
+
+    return score;
 }
 
 fn main() {
-    let mut score: u32 = 0;
-
     let data = fs::read_to_string("./inp/d2.txt").unwrap();
-
     let decisions = data
         .lines()
         .map(|line| line.split(" ").collect::<Vec<&str>>())
         .map(|a| Decision(RPC::new_from_str(a[0]), RPC::new_from_str(a[1])))
         .collect::<Vec<_>>();
-
-    for decision in decisions {
-        score += decision.1 as u32 + decision.decision();
-    }
-
+    let score = part_1(decisions.clone());
+    println!("{}", score);
+    let score = part_2(decisions.clone());
     println!("{}", score);
 }
