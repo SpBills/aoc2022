2c9b8030ca84975f10b89a7a3db72b704a4aecde
SpBills <spencebills0@gmail.com>
2022-12-03 15:45:19 -0500

    d3p1

--- d3/src/main.rs
+++ d3/src/main.rs
@@ -1,8 +1,38 @@
 use std::fs;
 
+#[derive(Debug, Clone, Copy)]
+struct Valuable(char, u32);
+
+impl Valuable {
+    fn new(c: char) -> Self {
+        if c.is_uppercase() {
+            return Self(c, c as u32 - 38)
+        } else {
+            return Self(c, c as u32 - 96)
+        }
+    }
+}
+
 // n collections of m characters
 #[derive(Debug)]
-struct Rucksack(Vec<Vec<char>>);
+struct Rucksack(Vec<Vec<Valuable>>);
+
+impl Rucksack {
+    fn find_dup_sum(&self) -> Option<Valuable> {
+        let first = &self.0[0];
+        let second = &self.0[1];
+
+        for x in first {
+            for y in second {
+                if x.0 == y.0 {
+                    return Some(x.clone());
+                }
+            }
+        }
+
+        None
+    }
+}
 
 fn split_nth(ruck_str: String, n: usize) -> Rucksack {
     let slice = ruck_str.chars().collect::<Vec<_>>();
@@ -13,8 +43,8 @@ fn split_nth(ruck_str: String, n: usize) -> Rucksack {
     let c = slice[..split]
         .chunks(quo + 1)
         .chain(slice[split..].chunks(quo))
-        .map(|s| s.iter().cloned().collect::<Vec<char>>())
-        .collect::<Vec<Vec<char>>>();
+        .map(|s| s.iter().map(|c| Valuable::new(*c)).collect::<Vec<Valuable>>())
+        .collect::<Vec<Vec<Valuable>>>();
 
     return Rucksack(c);
 }
@@ -27,5 +57,7 @@ fn main() {
         .map(|r| split_nth(r.to_owned(), 2))
         .collect::<Vec<Rucksack>>();
 
-    dbg!(sacks);
+    let sum: u32 = sacks.iter().map(|s| s.find_dup_sum().unwrap().1).sum();
+
+    println!("{}", sum);
 }
