c020bffdef969e190da5c7b13a19a379718a714b
spbills <spencebills0@gmail.com>
2022-12-04 14:29:54 -0500

    Renamed variables

--- d4/src/main.rs
+++ d4/src/main.rs
@@ -1,39 +1,35 @@
 use std::fs;
 
-use std::collections::HashSet;
-use std::hash::Hash;
-
 fn main() {
     let s = fs::read_to_string("./inp/d4.txt").unwrap();
 
-    let mut sum_1 = 0u32;
-    let mut sum_2 = 0u32;
+    let mut sum_one = 0u32;
+    let mut sum_two = 0u32;
     for line in s.lines() {
-        let split_line = line.split(",").collect::<Vec<_>>();
+        let range_halves = line.split(",").collect::<Vec<_>>();
 
-        let a = split_line[0];
+        let h1 = range_halves[0];
 
-        let c = a.split("-").collect::<Vec<_>>();
+        let h1_bounds = h1.split("-").collect::<Vec<_>>();
 
-        let c_0 = c[0].parse::<u32>().unwrap();
-        let c_1 = c[1].parse::<u32>().unwrap();
+        let h1_lower: u32 = h1_bounds[0].parse().unwrap();
+        let h1_upper = h1_bounds[1].parse().unwrap();
 
-        let b = split_line[1];
+        let h2 = range_halves[1];
         
-        let d = b.split("-").collect::<Vec<_>>();
+        let h2_bounds = h2.split("-").collect::<Vec<_>>();
 
-        let d_0 = d[0].parse::<u32>().unwrap();
-        let d_1 = d[1].parse::<u32>().unwrap();
+        let h2_lower = h2_bounds[0].parse().unwrap();
+        let h2_upper = h2_bounds[1].parse().unwrap();
 
-        if (c_0 >= d_0 && c_1 <= d_1) || (d_0 >= c_0 && d_1 <= c_1) {
-            sum_1 += 1;
+        if (h1_lower >= h2_lower && h1_upper <= h2_upper) || (h2_lower >= h1_lower && h2_upper <= h1_upper) {
+            sum_one += 1;
         }
-
-        if c_0 <= d_1 && d_0 <= c_1 {
-            sum_2 += 1;
+        if h1_lower <= h2_upper && h2_lower <= h1_upper {
+            sum_two += 1;
         }
     }
 
-    println!("{}", sum_1);
-    println!("{}", sum_2);
+    println!("Part 1: {}", sum_one);
+    println!("Part 2: {}", sum_two);
 }
