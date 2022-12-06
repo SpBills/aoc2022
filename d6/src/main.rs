ff7336e22d3b6bc9ef4774e8033894db92f9c799
spbills <spencebills0@gmail.com>
2022-12-06 09:29:14 -0500

    d6p2

--- d6/src/main.rs
+++ d6/src/main.rs
@@ -13,11 +13,26 @@ fn main() {
             let mut p = c.to_owned();
             p.sort();
             let part = p.partition_dedup();
-            dbg!(&part);
             (i+4, part.1.len() == 0)
         })
         .filter(|(_, b)| !!b)
         .collect::<Vec<_>>();
 
-    dbg!(window[0]);
+    println!("Part 1 found at {}", window[0].0);
+
+    let window = data
+        .chars()
+        .collect::<Vec<_>>()
+        .windows(14)
+        .enumerate()
+        .map(|(i, c)| {
+            let mut p = c.to_owned();
+            p.sort();
+            let part = p.partition_dedup();
+            (i+14, part.1.len() == 0)
+        })
+        .filter(|(_, b)| !!b)
+        .collect::<Vec<_>>();
+
+    println!("Part 2 found at {}", window[0].0);
 }
