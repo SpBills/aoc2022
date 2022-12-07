564374fbd9c33938859837e8659fc06bac8f8fd0
SpBills <spencebills0@gmail.com>
2022-12-07 08:06:12 -0500

    opti

--- d6/src/main.rs
+++ d6/src/main.rs
@@ -1,38 +1,27 @@
 #![feature(slice_partition_dedup)]
 
-use std::fs;
+use std::{collections::HashSet, fs};
 
 fn main() {
     let data = fs::read_to_string("./inp/d6.txt").unwrap();
     let window = data
-        .chars()
-        .collect::<Vec<_>>()
+        .as_bytes()
         .windows(4)
         .enumerate()
-        .map(|(i, c)| {
-            let mut p = c.to_owned();
-            p.sort();
-            let part = p.partition_dedup();
-            (i+4, part.1.len() == 0)
-        })
-        .filter(|(_, b)| *b)
-        .collect::<Vec<_>>();
+        .map(|(i, c)| (i + 4, c.iter().collect::<HashSet<_>>().len() == c.len()))
+        .find(|(_, b)| *b)
+        .unwrap();
 
-    println!("Part 1 found at {}", window[0].0);
+    println!("Part 1 found at {}", window.0);
 
     let window = data
         .chars()
         .collect::<Vec<_>>()
         .windows(14)
         .enumerate()
-        .map(|(i, c)| {
-            let mut p = c.to_owned();
-            p.sort();
-            let part = p.partition_dedup();
-            (i+14, part.1.len() == 0)
-        })
-        .filter(|(_, b)| *b)
-        .collect::<Vec<_>>();
+        .map(|(i, c)| (i + 14, c.iter().collect::<HashSet<_>>().len() == c.len()))
+        .find(|(_, b)| *b)
+        .unwrap();
 
-    println!("Part 2 found at {}", window[0].0);
+    println!("Part 2 found at {}", window.0);
 }
