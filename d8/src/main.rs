fb5e462602860df4ea9763162d9c8d6effde8000
spbills <spencebills0@gmail.com>
2022-12-08 12:29:29 -0500

    d7p1

--- d8/src/main.rs
+++ d8/src/main.rs
@@ -5,10 +5,10 @@ enum Dir {
     Up,
     Right,
     Down,
-    Left
+    Left,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Node {
     pos: usize,
     height: usize,
@@ -20,7 +20,7 @@ impl Node {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Graph {
     /// row length
     row: usize,
@@ -47,21 +47,27 @@ impl Graph {
             return None;
         }
         let pos = n.pos.checked_sub(1)?;
-        
 
         self.nodes.get(pos)
     }
 
     fn right(&self, n: &Node) -> Option<&Node> {
-        if n.pos % (self.row - 1) == 0 {
+        if n.pos % self.row == 98 {
             return None;
         }
         self.nodes.get(n.pos + 1)
     }
 }
 
-fn node_is_visible_dir(G: &Graph, n: &Node, dir: Dir) -> bool {
-    let next_maybe: Option<&Node> = match dir {
+/// `G` is our search graph
+///
+/// `n` is our node to find the next of.
+///
+/// `orig` tracks the original node to do comparisons
+///
+/// `dir` gives us the direction to work off of
+fn node_is_visible_dir(G: &Graph, n: &Node, orig: &Node, dir: Dir) -> bool {
+    let next_maybe = match dir {
         Dir::Up => G.up(n),
         Dir::Right => G.right(n),
         Dir::Down => G.down(n),
@@ -72,26 +78,40 @@ fn node_is_visible_dir(G: &Graph, n: &Node, dir: Dir) -> bool {
         return true;
     };
 
-    if next.height > n.height {
+    if next.height >= orig.height {
         return false;
     }
 
-    node_is_visible_dir(G, next, dir)
+    node_is_visible_dir(G, next, orig, dir)
 }
 
 fn main() {
     let data = fs::read_to_string("./inp/d8.txt").unwrap();
 
     let row_size = data.lines().peekable().peek().unwrap().len();
+
     let correct_data = data.replace(&['\n', '\r'][..], "");
+
     let nodes = correct_data
         .chars()
         .enumerate()
         .map(|(i, c)| Node::new(i, c.to_digit(10).unwrap() as usize))
         .collect::<Vec<_>>();
+
     let G: Graph = Graph::new(row_size, nodes);
 
-    let num = G.nodes.iter().filter(|n| [Dir::Up, Dir::Right, Dir::Down, Dir::Left].iter().any(|dir| node_is_visible_dir(&G, n, *dir))).collect::<Vec<_>>().len();
+    // G.nodes.iter().for_each(|n| print!("({},{})", n.c, n.pos));
+
+    let num = G
+        .nodes
+        .iter()
+        .filter(|n| {
+            [Dir::Up, Dir::Right, Dir::Down, Dir::Left]
+                .into_iter()
+                .any(|dir| node_is_visible_dir(&G, n, n, dir))
+        })
+        .collect::<Vec<_>>()
+        .len();
 
     println!("{}", num)
 }
